7d1e62d771eac24f2218c7ff30a51a6396aa745f
Thenilde Borges <thenilde.tb@hotmail.com>
2023-11-11 22:31:04 +0000

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Departament;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Departament obj=new Departament(1 ," Books");
-		
-		Seller seller =new Seller( 1 ,"Thenilde", "thenilde@hotmail.com", new Date(),3000.0, obj);
 		
 		SellerDao sellerDao =DaoFactory.createSellerDao();
 		
+		Seller seller =sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
